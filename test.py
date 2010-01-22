f4d84f8ccd35e65792b63255c5c68cc2bd123964
John Stowers <john.stowers@gmail.com>
2010-01-22 16:43:19 +0100

    Mae GUDeviceNumber and init as per comment

--- test.py
+++ test.py
@@ -8,17 +8,17 @@ import gudev
 import glib
 
 def print_device(device):
-    print "subsystem", device.get_subsystem ()
-    print "devtype", device.get_devtype ()
-    print "name", device.get_name ()
-    print "number", device.get_number ()
-    print "sysfs_path:", device.get_sysfs_path ()
-    print "driver:", device.get_driver ()
-    print "action:", device.get_action ()
-    print "seqnum:", device.get_seqnum ()
-    print "device type:", device.get_device_type ()
-#    print "device number:", device.get_device_number ()
-    print "device file:", device.get_device_file ()
+    print "subsystem", device.get_subsystem()
+    print "devtype", device.get_devtype()
+    print "name", device.get_name()
+    print "number", device.get_number()
+    print "sysfs_path:", device.get_sysfs_path()
+    print "driver:", device.get_driver()
+    print "action:", device.get_action()
+    print "seqnum:", device.get_seqnum()
+    print "device type:", device.get_device_type()
+    print "device number:", device.get_device_number()
+    print "device file:", device.get_device_file()
 
 def on_uevent(client, action, device):
     print "UEVENT"
